d3353c95ad0db7f698cda63d9210608b3bc3963c
Shusaku-y <rsk9xmjnpg11zsreprit@outlook.jp>
2025-05-11 16:08:13 +0900

    最初のコミット

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">My Todo App</h1>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '~/stores/counter'
+
+const counter = useCounterStore()
+</script>
