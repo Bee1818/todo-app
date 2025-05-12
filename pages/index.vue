27a89c6dcc9fdb55830f3c6752a8208606e2deba
Shusaku-y <rsk9xmjnpg11zsreprit@outlook.jp>
2025-05-12 19:31:14 +0900

    削除ボタンのレイアウト調整

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,9 @@
     <ul class="w-4/5">
       <li
       v-for="(todo, index) in todos" 
-      class="flex justify-between items-center">
-        <p>{{ todo }}</p>
-        <div class="flex gap-[10px] w-[100px]">
+      class="list-none">
+        <p class="w-5/6">{{ todo }}</p>
+        <div class="flex justify-end gap-[10px]">
           <button class="button edit block">編集</button>
           <button @click="deleteTodo(index)" class="button delete block">削除</button>
         </div>
