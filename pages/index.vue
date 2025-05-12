e6c3a010830370c2e7c8ad89993cb64c267705d5
Shusaku-y <rsk9xmjnpg11zsreprit@outlook.jp>
2025-05-12 17:12:07 +0900

    ページ作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="container">
-    <h1 class="text-2xl font-bold title">My Todo App</h1>
-    <h1 class="text-3xl font-bold">Tailwind 有効化完了！</h1>
+  <div class="text-center">
+    <h1 class="">TODOリスト</h1>
+    <form action="" class="w-4/5 mx-auto flex gap-[10px]">
+      <input type="text" placeholder="新しいメモ入力" class="block w-4/5">
+      <button type="submit" class="button add block">追加</button>
+    </form>
+    <ul class="w-4/5">
+      <li class="flex justify-between items-center">
+        <p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+        <div class="flex gap-[10px] w-[100px]">
+          <button class="button edit block">編集</button>
+          <button class="button delete block">削除</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
 </script>
+
 <style scoped>
-.container {
-  background-color: antiquewhite;
+.button {
+  border: solid 1px black;
+  border-radius: 5px;
+  cursor: pointer;
+  color: white;
 }
-.title {
-  color: blue;
+.add {
+  background-color: blue;
+}
+.edit {
+  background-color: green;
+}
+.delete {
+  background-color: red;
 }
 </style>
